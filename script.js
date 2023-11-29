d218c27089b964fceb79175245c127716cdf62d5
robie081 <robert.lantus@icloud.com>
2023-11-29 16:01:21 +0200

    pilates

--- script.js
+++ script.js
@@ -1,45 +1,26 @@
 
-// Retrieved from menus/afterEd for next dev
+// Pilates Body & Face Copou
 
 
-
-
-const navSlide = () => {
-    const hamburger = document.querySelector('.hamburger');
+const pilates = () => {
+    const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
 
     // Hamburger Animation and Nav Toggle
 
-    hamburger.addEventListener('click', () => {
-        hamburger.classList.toggle('change');
-        nav.classList.toggle('nav-active');
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('change');
 
-        // Animate Links
-        navLinks.forEach((link, index) => {
-        // console.log(index);
-        // console.log(index / 7);
-        if (link.style.animation)
-            link.style.animation = '';
+        if (nav.style.width === '400px')
+            nav.style.width = '0px';
         else
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        });
+            nav.style.width = '400px';
     });
 }
 
-navSlide();
+pilates();
 
 
 
-// TEROX Travel Hamburger
 
-// const hamburgerMenu = () => {
-//     const hamburger = document.querySelector('.hamburger');
-
-//     hamburger.addEventListener('click', () => {
-//         hamburger.classList.toggle('change');
-//     });
-// }
-
-// hamburgerMenu();
 
